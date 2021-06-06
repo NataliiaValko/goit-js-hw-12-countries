02328144f7e915ac8201515b552a653f7e365a5a
Nataliia Valko <tanarisoffice@gmail.com>
2021-06-06 18:08:18 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './sass/main.scss';
+import '@pnotify/core/dist/BrightTheme.css';
+
+import './js/refs';
+import './js/countries.js';
+import './js/fetchCountries';
+import './js/renderMarkupCountry';
+
---
+++ src/js/refs.js
@@ -0,0 +1,4 @@
+export default {
+    input: document.querySelector('.input-country'),
+    result: document.querySelector('.result'),
+}
